77f852908fe879012b29ae450474b895dd4ba41a
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-27 11:34:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css"
+
+function App(){
+    return (
+      <div className="container">
+      <div className="el1"><b>Header</b></div>
+      <div className="el2"><b>Nav</b></div>
+      <div className="el3"><b>Article</b></div>
+      <div className="el4"><b>Ads</b></div>
+      <div className="el5"><b>Footer</b></div>
+      </div>
+    );
+  }
+
+export default App;
